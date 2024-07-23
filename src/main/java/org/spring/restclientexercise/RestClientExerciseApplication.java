9ac271500036947700b421329289f57496296dd3
Simon Staß <simonsta95@gmail.com>
2024-07-23 14:52:22 +0200

    Initial commit

---
+++ src/main/java/org/spring/restclientexercise/RestClientExerciseApplication.java
@@ -0,0 +1,13 @@
+package org.spring.restclientexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/spring/restclientexercise/RestClientExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.restclientexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
